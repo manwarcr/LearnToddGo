7e3a9fa4dc67a9c7d6dfed833d5c3f3ff30341da
manwarcr <chrisman504@gmail.com>
2025-01-06 08:42:25 +0000

    Update greetings.go

--- UdemyGo/greetings.go
+++ UdemyGo/greetings.go
@@ -2,6 +2,6 @@ package greetings
 
 import "fmt"
 
-func greetings(st string) {
-  fmt.Printf("Hello %s, welcome to the course",st)
+func HelloGit(st string) {
+  fmt.Printf("Hello %s, welcome to Github,st)
 }
